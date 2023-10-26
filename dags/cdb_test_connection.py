a1a5ca52f531d7dfe925a74925ab80dc44ffd2ab
Rajasuryaa Palanisamy <rajasuryaapalanisamy@FVFGW15MQ05N.local>
2023-10-26 15:40:03 +0530

    added test connection dag

---
+++ dags/cdb_test_connection.py
@@ -0,0 +1,39 @@
+"""
+Implements a retention policy by dropping expired partitions
+
+A detailed tutorial is available at https://community.crate.io/t/cratedb-and-apache-airflow-implementation-of-data-retention-policy/913
+
+Prerequisites
+-------------
+In CrateDB, tables for storing retention policies need to be created once manually.
+See the file setup/data_retention_schema.sql in this repository.
+"""
+from pathlib import Path
+import pendulum
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.decorators import dag, task
+from airflow.operators.empty import EmptyOperator
+
+
+@task
+def get_policies():
+   SQLExecuteQueryOperator(
+            task_id=f"copy_{export_table['table']}",
+            conn_id="cratedb_connection",
+            sql="""
+                    select count(*) from keywords_partitioned;
+                """)
+@dag(
+    start_date=pendulum.datetime(2021, 11, 19, tz="UTC"),
+    schedule="@daily",
+    catchup=False,
+)
+def data_retention_delete_test():
+    start = EmptyOperator(task_id="start")
+    end = EmptyOperator(task_id="end")
+    tg1 = get_policies()
+
+
+
+data_retention_delete_test()
