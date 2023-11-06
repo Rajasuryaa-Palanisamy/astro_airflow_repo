00096225d5cbd67783851de5d102257a8c2f34c0
Rajasuryaa Palanisamy <98268784+Rajasuryaa-Palanisamy@users.noreply.github.com>
2023-11-06 13:57:56 +0530

    Update cdb_ttl_wo_partition_dag.py

--- dags/cdb_ttl_wo_partition_dag.py
+++ dags/cdb_ttl_wo_partition_dag.py
@@ -1,24 +1,30 @@
-
 from pathlib import Path
 import pendulum
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.decorators import dag, task
+from airflow.decorators import dag, task_group
+from airflow.operators.empty import EmptyOperator
 
 
+@task_group
+def get_policies():
+   print("entered sql query op function")
+   SQLExecuteQueryOperator(
+            task_id="test_cdb_connection",
+            conn_id="cratedb_connection",
+            sql="""
+                    DELETE from keywords_random_ip_ttl_test_min WHERE TIMESTAMP > 1699170005565;
+                """)
 @dag(
-    start_date=pendulum.datetime(2023, 11, 19, tz="UTC"),
+    start_date=pendulum.datetime(2022, 11, 19, tz="UTC"),
     schedule="@daily",
     catchup=False,
 )
-
-@task
 def ttl_delete_test():
-    SQLExecuteQueryOperator.partial(
-        task_id="delete_partition",
-        conn_id="cratedb_connection",
-        sql="DELETE from keywords_random_ip_ttl_test_min WHERE TIMESTAMP > 1699170005565;",
-    )
+    start = EmptyOperator(task_id="start")
+    end = EmptyOperator(task_id="end")
+    tg1 = get_policies()
+
 
 
 ttl_delete_test()
