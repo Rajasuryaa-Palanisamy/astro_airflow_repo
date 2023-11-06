7de150c06f975d8c18f735a5a29c65bc9552b758
Rajasuryaa Palanisamy <rajasuryaapalanisamy@FVFGW15MQ05N.local>
2023-11-06 22:24:29 +0530

    asc

---
+++ dags/cdb_ttl_with_parititon_dag.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import pendulum
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.decorators import dag, task_group
+from airflow.operators.empty import EmptyOperator
+
+
+@task_group
+def get_policies():
+   print("entered sql query op function")
+   SQLExecuteQueryOperator(
+            task_id="test_cdb_connection",
+            conn_id="cratedb_connection",
+            sql="""
+                    DELETE from keywords_paritioned_ttl_test_2_data_new WHERE TIMESTAMP < 1699288677285;
+                """)
+@dag(
+    start_date=pendulum.datetime(2022, 11, 19, tz="UTC"),
+    schedule="@daily",
+    catchup=False,
+)
+def ttl_delete_with_part_test():
+    start = EmptyOperator(task_id="start")
+    end = EmptyOperator(task_id="end")
+    tg1 = get_policies()
+
+
+
+ttl_delete_with_part_test()
