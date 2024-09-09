c189501b2a545ff311335e0d6e1d1f377cdc99aa
Rajasuryaa Palanisamy <rajasuryaa.palanisamy@searce.com>
2024-09-09 14:32:47 +0530

    salesforce added

---
+++ dags/sql_query_dag.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.operators.sql import SQLExecuteQueryOperator
+from airflow.utils.dates import days_ago
+from datetime import timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    'example_mysql_dag',
+    default_args=default_args,
+    description='A simple DAG to query MySQL',
+    schedule_interval=None,
+    start_date=days_ago(1),
+    catchup=False,
+) as dag:
+
+    query_task = SQLExecuteQueryOperator(
+        task_id='execute_mysql_query',
+        sql='SELECT * FROM employees;',
+        mysql_conn_id='mysql_default',
+    )
+
+    query_task
