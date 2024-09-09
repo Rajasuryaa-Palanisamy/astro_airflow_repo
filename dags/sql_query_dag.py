350585d9df0b2414b6f1fb6b462b4afe6a224cc3
Rajasuryaa Palanisamy <rajasuryaa.palanisamy@searce.com>
2024-09-09 14:43:51 +0530

    salesforce added

--- dags/sql_query_dag.py
+++ dags/sql_query_dag.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.utils.dates import days_ago
 from datetime import timedelta
+import logging
 
 default_args = {
     'owner': 'airflow',
@@ -10,6 +11,21 @@ default_args = {
     'email_on_retry': False,
     'retries': 0
 }
+
+
+# Function to fetch and print data from XCom
+def print_query_results(**kwargs):
+    # Fetch the query results from XCom
+    ti = kwargs['ti']
+    query_results = ti.xcom_pull(task_ids='execute_sql_query')
+    
+    # Print the results to Airflow logs
+    if query_results:
+        logging.info("Query Results:")
+        for row in query_results:
+            logging.info(row)
+    else:
+        logging.info("No results found or query failed.")
 
 with DAG(
     'mysql_dag',
@@ -25,7 +41,15 @@ with DAG(
         sql='SELECT * FROM employees;',
         conn_id='mysql_default',
         return_last=True,
-        show_return_value_in_logs=True
+        show_return_value_in_logs=True,
+        do_xcom_push=True,
     )
 
-    query_task
+    # Task to print query results to Airflow logs
+    print_results = PythonOperator(
+        task_id='print_query_results',
+        python_callable=print_query_results,
+        provide_context=True,
+    )
+
+    query_task >> print_results
